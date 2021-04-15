9107b321ce98df948c1da2b37c51ce45b009536a
Mohamad  Al Sabaa <mohamadsabaa3@gmail.com>
2021-04-15 10:29:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -39,13 +39,13 @@ function wakeDog(dogName, dogBreed) {
 // const n6 = unleashDog();
 
 //const routine = [n1, n2, n3, n4, n5, n6];
-let routine = [wakeDog,leashDog,walkToPark,throwFrisbee,walkHome,unleashDog];
-let newArr = [];
+
+
 
 function exerciseDog (dogName, dogBreed){
+  let routine = [wakeDog,leashDog,walkToPark,throwFrisbee,walkHome,unleashDog];
   for (let i=0; i<routine.length; i++){
-    newArr.push(routine[i](dogName, dogBreed));
-    return  newArr;
+    return routine[i](dogName, dogBreed);
   }
 }
 exerciseDog("Esther", "Dalmation");
